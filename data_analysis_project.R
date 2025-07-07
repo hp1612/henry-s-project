cc5da68ab63a8fba80a10c2dbb0c0cd0f480b0da
hp1612 <144681783+hp1612@users.noreply.github.com>
2025-07-07 17:26:18 +0800

    Delete data_analysis_project.R

--- data_analysis_project.R
+++
@@ -1,6 +0,0 @@
-library(readr)
-df <- read_csv("C:/Users/85251/Downloads/gdp_versus_average_transfer_fee (1).csv")
-pairs(df[, c(2, 3, 4)], main = "Inter-relationship Plot Matrix")
-summary(lm(avg_fee ~ gdp_UK, data = df))
-summary(lm(avg_fee ~ gdp_World, data = df))
-
